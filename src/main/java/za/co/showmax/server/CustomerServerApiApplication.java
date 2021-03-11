0089b4e32ea3e493f297db9e0025aadb6fcc77e3
MARK MNGOMA <markmngoma@MARKs-MacBook-Pro.local>
2021-03-11 21:47:25 +0200

    setup repo

---
+++ src/main/java/za/co/showmax/server/CustomerServerApiApplication.java
@@ -0,0 +1,13 @@
+package za.co.showmax.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/showmax/server/CustomerServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.showmax.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
